bbe4adc1ab84b5b0b142f0cc096e83aca170c3b8
Adam <Adam@Natashas-MacBook.local>
2013-07-10 11:08:02 +0100

    added authorisation

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,10 +4,10 @@ class Ability
   def initialize(user)
    user ||= User.new
    case
-    when user.role?(:admin)
+    when user.role?("admin")
     can :manage, :all
 
-    when user.role?(:employee)
+    when user.role?("employee")
     can :read, :all
 
     else
